19d38e918d9f49a545ccf73d600ccb4ffc4fe219
Abhijeet Kasurde <akasurde@redhat.com>
2017-08-16 18:53:15 +0530

    Modified build.sh
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- build.sh
+++ build.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-set -x
+rhel_ver=${1:-7.4}
+echo "Building Virtual Machine using virt-install for ${rhel_ver}"
 
-#tree=../../rhel-server-7.2-x86_64-dvd.iso
-tree=http://download.eng.bos.redhat.com/composes/released/RHEL-7/7.2/Server/x86_64/os/
+ks_file="rhel-${rhel_ver}-ci-cloud_edit.ks"
+disk_name="rhel-vagrant-${rhel_ver}.x86_64.qcow2"
+box_name="rhel-${rhel_ver}.x86_64.box"
+vm_name="rhel-vm-${rhel_ver}.x86_64.box"
+temp_dir="`pwd`/tmp"
 
+tree=http://download.eng.bos.redhat.com/composes/released/RHEL-7/${rhel_ver}/Server/x86_64/os/
+
+if [ ! -f ${ks_file} ]; then
+    cp template_kickstart.ks ${ks_file}
+fi
+
+echo "Installing virtual machine using ${tree}\n"
 virt-install --connect=qemu:///system \
     --network=bridge:virbr0 \
-    --initrd-inject=./rhel-7.2-ci-cloud_edit.ks \
-    --extra-args="ks=file:/rhel-7.2-ci-cloud_edit.ks no_timer_check console=tty0 console=ttyS0,115200 net.ifnames=0 biosdevname=0" \
-    --name=rhel-vagrant-7.2.x86_64 \
-    --disk ./rhel-vagrant-7.2.x86_64.qcow2,size=20,bus=virtio \
+    --initrd-inject=./${ks_file} \
+    --extra-args="ks=file:/${ks_file} no_timer_check console=tty0 console=ttyS0,115200 net.ifnames=0 biosdevname=0" \
+    --name=${vm_name} \
+    --disk ./${disk_name},size=20,bus=virtio \
     --ram 2048 \
     --vcpus=2 \
     --check-cpu \
@@ -18,6 +29,8 @@ virt-install --connect=qemu:///system \
     --location=$tree \
     --nographics --noreboot #--debug 
 
-LIBGUESTFS_BACKEND=direct virt-sparsify --compress -o compat=0.10 --tmp ./tmp/ rhel-vagrant-7.2.x86_64.qcow2 box.img
+echo "Compressing image"
+LIBGUESTFS_BACKEND=direct virt-sparsify --compress -o compat=0.10 --tmp ${temp_dir} ${disk_name} box.img
 
-tar cvzf rhel72.box ./metadata.json ./Vagrantfile ./box.img
+echo "Creating box image: ${box_name}"
+tar cvzf ${box_name} ./metadata.json ./Vagrantfile ./box.img
